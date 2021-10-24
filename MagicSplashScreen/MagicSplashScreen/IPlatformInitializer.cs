d7142b7c9afa9fccb35251abb379e17de3a66bac
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2021-10-24 17:18:22 +0200

    iOS Splash Screen

--- MagicSplashScreen/MagicSplashScreen/IPlatformInitializer.cs
+++ MagicSplashScreen/MagicSplashScreen/IPlatformInitializer.cs
@@ -2,8 +2,15 @@ using System.Threading.Tasks;
 
 namespace MagicSplashScreen
 {
+    /// <summary>
+    /// Platform Initializer
+    /// </summary>
     public interface IPlatformInitializer
     {
+        /// <summary>
+        /// Perform all platform specific initialisation
+        /// </summary>
+        /// <returns>awaitable task</returns>
         Task Initialize();
     }
 }
