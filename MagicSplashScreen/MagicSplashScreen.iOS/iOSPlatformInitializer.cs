cc3f6e50eeb907d43a8de6db61f02ac9d32d8ebb
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2021-10-24 17:20:38 +0200

    Add Summary for iOS

--- MagicSplashScreen/MagicSplashScreen.iOS/iOSPlatformInitializer.cs
+++ MagicSplashScreen/MagicSplashScreen.iOS/iOSPlatformInitializer.cs
@@ -3,9 +3,15 @@ using System.Threading.Tasks;
 
 namespace MagicSplashScreen.iOS
 {
+    /// <summary>
+    /// iOS Platform Initializer
+    /// </summary>
     // ReSharper disable once InconsistentNaming
     public class iOSPlatformInitializer : IPlatformInitializer
     {
+        /// <summary>
+        /// Perform all platform specific initialisation
+        /// </summary>
         public Task Initialize()
         {
             //initialize all nuget packages that is possible initialize later
